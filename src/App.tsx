d116e079ff6e3be6ea9b2edcdf2ad5c147a6c9a0
Nathanael Mathieu <nmathieu@wesleyan.edu>
2022-04-10 14:09:57 -0400

    Should have used flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function scoreboardRow(teamName: string) {
+  return (
+    <>
+    <div className={teamName + " scoreboardRow"}>
+      <div className="card player-number">00</div>
+      <div className="card team-name">{teamName}</div>
+      <div className="card">1</div>
+      <div className="card">2</div>
+      <div className="card">3</div>
+      <div className="spacer"></div>
+      <div className="card">4</div>
+      <div className="card">5</div>
+      <div className="card">6</div>
+      <div className="spacer"></div>
+      <div className="card">7</div>
+      <div className="card">8</div>
+      <div className="card">9</div>
+      <div className="spacer"></div>
+      <div className="card">10</div>
+      <div className="spacer"></div>
+      <div className="spacer"></div>
+      <div className="card">R</div>
+      <div className="spacer"></div>
+      <div className="card">H</div>
+      <div className="spacer"></div>
+      <div className="card">E</div>
+      <div className="spacer"></div>
+    </div>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {scoreboardRow("")}
+      {scoreboardRow("VISITOR")}
+      {scoreboardRow("BOSTON")}
     </div>
   );
 }
